93d0f66a9c924b3eb6f0c278f704baad2548f750
Sung-il Kang <oniono@naver.com>
2016-09-04 22:07:56 +0900

    Peer Graded Assignment: Course Project 1

---
+++ plot2.R
@@ -0,0 +1,13 @@
+setwd("C:\\DataScience\\4.EDA")
+
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time), "%d/%m/%Y %H:%M:%S") 
+global_active_power <- as.numeric(subSetData$Global_active_power)
+
+png("plot2.png", width=480, height=480)
+plot(datetime, global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,18 @@
+setwd("C:\\DataScience\\4.EDA")
+
+dataFile <- "household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(subSetData$Date, subSetData$Time), "%d/%m/%Y %H:%M:%S") 
+global_active_power <- as.numeric(subSetData$Global_active_power)
+sub_Metering1 <- as.numeric(subSetData$Sub_metering_1)
+sub_Metering2 <- as.numeric(subSetData$Sub_metering_2)
+sub_Metering3 <- as.numeric(subSetData$Sub_metering_3)
+
+png("plot3.png", width=480, height=480)
+plot(datetime, sub_Metering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, sub_Metering2, type="l", col="red")
+lines(datetime, sub_Metering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
